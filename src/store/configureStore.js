7b1be72bc5648a2e571eca78c4c3c55f38cd7bac
Sander Houttekier <sander.houttekier@gmail.com>
2016-06-25 15:52:32 +0200

    Load courses on app start

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,9 +1,11 @@
 import {createStore, applyMiddleware, compose} from 'redux';
 import rootReducer from '../reducers';
 import reduxImmutableStateInvariant from 'redux-immutable-state-invariant';
+import thunk from 'redux-thunk';
 
 export default function configureStore(initialState) {
   const middleware = [
+    thunk,
     reduxImmutableStateInvariant()
   ];
 
