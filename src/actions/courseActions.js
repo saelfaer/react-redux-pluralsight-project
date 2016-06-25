10600b407c5558375c43358fd7f6ed3038cd247d
Sander Houttekier <sander.houttekier@gmail.com>
2016-06-25 19:46:45 +0200

    Prefil courses form from state

--- src/actions/courseActions.js
+++ src/actions/courseActions.js
@@ -2,10 +2,15 @@ import * as types from './actionTypes';
 import courseApi from '../api/mockCourseApi';
 
 export function loadCoursesSuccess(courses) {
-  return {
-    type: types.LOAD_COURSES_SUCCESS,
-    courses
-  };
+  return { type: types.LOAD_COURSES_SUCCESS, courses };
+}
+
+export function updateCourseSuccess(course) {
+  return { type: types.UPDATE_COURSE_SUCCESS, course };
+}
+
+export function createCourseSuccess(course) {
+  return { type: types.CREATE_COURSE_SUCCESS, course };
 }
 
 export function loadCourses() {
@@ -13,6 +18,22 @@ export function loadCourses() {
     return courseApi.getAllCourses()
       .then(courses => {
         dispatch(loadCoursesSuccess(courses));
+      })
+      .catch(error => {
+        throw(error);
+      });
+  };
+}
+
+export function saveCourse(course) {
+  return (dispatch, getState) => {
+    return courseApi.saveCourse(course)
+      .then(savedCourse => {
+        if (course.id) {
+          dispatch(updateCourseSuccess(savedCourse));
+        } else {
+          dispatch(createCourseSuccess(savedCourse));
+        }
       })
       .catch(error => {
         throw(error);
--- src/components/course/ManageCoursePage.js
+++ src/components/course/ManageCoursePage.js
@@ -12,6 +12,28 @@ class ManageCoursePage extends Component {
       course: Object.assign({}, this.props.course),
       errors: {}
     };
+
+    this.saveCourse = this.saveCourse.bind(this);
+    this.updateCourseState = this.updateCourseState.bind(this);
+  }
+
+  componentWillReceiveProps(nextProps) {
+    if (this.props.course.id != nextProps.course.id) {
+      this.setState({course: Object.assign({}, nextProps.course)});
+    }
+  }
+
+  updateCourseState(event) {
+    const field = event.target.name;
+    let course = this.state.course;
+    course[field] = event.target.value;
+    return this.setState({course});
+  }
+
+  saveCourse(event) {
+    event.preventDefault();
+    this.props.actions.saveCourse(this.state.course);
+    this.context.router.push('/courses');
   }
 
   render() {
@@ -19,18 +41,35 @@ class ManageCoursePage extends Component {
       <CourseForm
         course={this.state.course}
         errors={this.state.errors}
-        allAuthors={this.props.authors}/>
+        allAuthors={this.props.authors}
+        onChange={this.updateCourseState}
+        onSave={this.saveCourse} />
     );
   }
 }
 
 ManageCoursePage.propTypes = {
   course: PropTypes.object.isRequired,
-  authors: PropTypes.array.isRequired
+  authors: PropTypes.array.isRequired,
+  actions: PropTypes.object.isRequired
 };
 
+ManageCoursePage.contextTypes = {
+  router: PropTypes.object
+};
+
+function getCourseById(courses, id) {
+  const course = courses.filter(course => course.id === id);
+  if (course.length) {
+    return course[0];
+  }
+  return null;
+}
+
 function mapStateToProps(state, ownProps) {
-  const course = {
+  const courseId = ownProps.params.id;
+
+  let course = {
     id: '',
     watchHref: '',
     title: '',
@@ -38,6 +77,10 @@ function mapStateToProps(state, ownProps) {
     length: '',
     category: ''
   };
+
+  if (courseId && state.courses.length > 0) {
+    course = getCourseById(state.courses, courseId);
+  }
 
   const authorsFormattedForDropdown = state.authors.map(author => {
     return {
