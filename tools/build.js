e6016eb68962605e874fdbb8627e12a620f2bdd3
Sander Houttekier <sander.houttekier@gmail.com>
2016-06-28 00:25:55 +0200

    Add build tool wo compile javascript

---
+++ tools/build.js
@@ -0,0 +1,35 @@
+// More info on Webpack/s Node API here: https://webpack.github.io/docs/node.js-api.html
+// Allowing console calls below since this is a build file.
+/*eslint-disable no-console*/
+import webpack from 'webpack';
+import webpackConfig from '../webpack.config.prod';
+import colors from 'colors';
+
+process.env.NODE_ENV = 'production'; // this assures the Babel dev config (for HMR) doesn't apply.
+
+console.log('Generating minified bundle for production via Webpack.\nThis will take a moment...'.blue);
+
+webpack(webpackConfig).run((err, stats) => {
+  if (err) {
+    console.log(err.bold.red);
+    return 1;
+  }
+
+  const jsonStats = stats.toJson();
+
+  if (jsonStats.hasErrors) {
+    return jsonStats.errors.map(error => console.log(error.red));
+  }
+
+  if (jsonStats.hasWarnings) {
+    console.log('Webpack generated the following warnings: '.bold.yellow);
+    return jsonStats.warnings.map(warning => console.log(warning.yellow));
+  }
+
+  console.log(`Webpack stats: ${stats}`);
+
+  // if we got this far, the build must have succeeded
+  console.log('Your app has been compiled in production mode and written to /dist. it\'s ready to roll!'.green);
+
+  return 0;
+});
